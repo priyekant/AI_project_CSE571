e2de30efc0728ce6c9be721b35f486a67ce6d752
priyekant <priyekantaghi@gmail.com>
2017-09-28 20:37:55 -0700

    project 3 complete

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -60,9 +60,9 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question6():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    answerEpsilon = 0.9
+    answerLearningRate = 0.1
+    return 'NOT POSSIBLE'
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
--- reinforcement/qlearningAgents.py
+++ reinforcement/qlearningAgents.py
@@ -200,14 +200,21 @@ class ApproximateQAgent(PacmanQAgent):
           where * is the dotProduct operator
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        features = self.featExtractor.getFeatures(state,action)
+        qvalue = 0
+        for feature in features:
+            qvalue += self.weights[feature] * features[feature]
+        return qvalue
 
     def update(self, state, action, nextState, reward):
         """
            Should update your weights based on transition
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        difference = (reward + self.discount * self.computeValueFromQValues(nextState)) - self.getQValue(state,action)
+        features = self.featExtractor.getFeatures(state,action)
+        for feature in features:
+            self.weights[feature] += self.alpha * difference * features[feature]
 
     def final(self, state):
         "Called at the end of each game."
